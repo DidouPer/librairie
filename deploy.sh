fd09053fdc810e2e7af39127a30b650793674d22
johann <you@example.com>
2017-11-23 15:51:05 +0000

    [travis] important git 2

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
-rev=$(git rev-parse --short HEAD)
 git config user.name "DidouPer"
 git config user.password "Didsd2011"
-git add .
-git commit -m "committed at ${rev}"
-git push origin HEAD:preprod
+git status
+git add --all
+git commit -m "committed"
+git push origin master
+git checkout preprod
+git pull origin preprod
+git push origin preprod
