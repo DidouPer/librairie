7c050c56c299a6ea3010c9c91bf0a6d80855b3ad
johann <you@example.com>
2017-11-23 14:51:13 +0000

    [travis] all tests deploy prperod

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rev=$(git rev-parse --short HEAD)
-git config user.name "jhocre"
-git config user.password "Jhorhcp091427"
+git config user.name "DidouPer"
+git config user.password "Didsd2011"
 git add .
 git commit -m "committed at ${rev}"
-git push origin HEAD:master
+git push origin HEAD:preprod
